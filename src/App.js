a2f98b01de5b2139b8cbcdfc96faa389f8b3d8b2
Tianhang Liu <tianhanl@usc.edu>
2018-03-26 22:32:50 -0700

    allow all the graphes be place in via configuration

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TabbedGraphConntainer from './components/TabbedGraphContainer';
-import ResultView from './ResultView';
-import PollingView from './PollingView';
+import ResultView from './components/ResultView';
+import PollingView from './components/PollingView';
 const tabNames = [
   'Pollings',
   'Results'
@@ -18,7 +18,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <TabbedGraphConntainer />
+        <TabbedGraphConntainer tabNames={tabNames} tabContents={tabContents} />
       </div>
     );
   }
