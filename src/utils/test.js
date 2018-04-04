14ff450b7d541543e7aeeacc70454acb997e2626
Tianhang Liu <tianhanl@usc.edu>
2018-04-04 01:36:10 -0700

    add final data

--- src/utils/test.js
+++ src/utils/test.js
@@ -1,17 +1,17 @@
 const { readCSVFromLocal } = require('./parse');
 const fs = require('fs');
 
-const filename = '/fianl_result.csv';
-const outputname = 'final_result.json';
+const filename = '/data/uk_final_data.csv';
+const outputname = 'uk_final_data.json';
 
 readCSVFromLocal(filename)
-    .then(results => {
-        console.log(results.data);
-        let content = JSON.stringify(results.data);
-        fs.writeFile(outputname, content, 'utf8', err => {
-            if (err) throw err;
-        });
-    })
-    .catch(error => {
-        console.log(error);
+  .then(results => {
+    console.log(results.data);
+    let content = JSON.stringify(results.data);
+    fs.writeFile(outputname, content, 'utf8', err => {
+      if (err) throw err;
     });
+  })
+  .catch(error => {
+    console.log(error);
+  });
